9a459349afb76fdc08dadae074efb430343872f3
Pedro Civita <pedro.civita@gmail.com>
2023-09-11 14:06:00 -0300

    Musicas separadas por structs

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -114,6 +114,12 @@
 
 // Melodia Mario
 
+typedef struct {
+	char* nome;
+	int* notasEDuracoes;
+	int tamanho;
+} Musica;
+
 int melody_mario[] = {
 
 	// Super Mario Bros theme
@@ -206,6 +212,8 @@ int melody_mario[] = {
 
 };
 
+Musica mario = {"Mario", melody_mario, sizeof(melody_mario)/sizeof(melody_mario[0])};
+
 // Funções
 void set_buzzer(){
 	pio_set(BUZZER_PIO_ID, BUZZER_PIO_IDX_MASK);
@@ -282,6 +290,7 @@ void init(void) {
 	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
 	
 	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+
 }
 
 int main (void){
@@ -303,36 +312,30 @@ int main (void){
 	// Init OLED
 	gfx_mono_ssd1306_init();
 	
+	// Musica atual
+	Musica musicaAtual = mario;
+	int tamanho = musicaAtual.tamanho;
+	
 	// Escreve na tela um circulo e um texto
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-	gfx_mono_draw_string("Issue 5", 50,16, &sysfont);
-	
+	gfx_mono_draw_string(musicaAtual.nome, 50,16, &sysfont);
 	
 	/* Insert application code here, after the board has been initialized. */
 	while(1) {
-	
-	for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+		for (int thisNote = 0; thisNote < tamanho; thisNote += 2) {
+			int note = musicaAtual.notasEDuracoes[thisNote];
+			int duracao = musicaAtual.notasEDuracoes[thisNote + 1];
+			divider = duracao;
+			
+			if (divider > 0) {
+				noteDuration = (wholenote) / divider;
+				} else if (divider < 0) {
+				noteDuration = (wholenote) / abs(divider);
+				noteDuration *= 1.5;
+			}
 
-		// calculates the duration of each note
-		divider = melody_mario[thisNote + 1];
-		if (divider > 0) {
-			// regular note, just proceed
-			noteDuration = (wholenote) / divider;
-			} else if (divider < 0) {
-			// dotted notes are represented with negative durations!!
-			noteDuration = (wholenote) / abs(divider);
-			noteDuration *= 1.5; // increases the duration in half for dotted notes
-		}
-
-		// we only play the note for 90% of the duration, leaving 10% as a pause
-		tone(melody_mario[thisNote], noteDuration*0.9);
-
-		// Wait for the specief duration before playing the next note.
-		delay_ms(noteDuration);
-
-		// stop the waveform generation before the next note.
-	
+			tone(note, noteDuration*0.9);
+			delay_ms(noteDuration);
 		}
 	}
 }
-
