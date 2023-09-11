ff2db07e3d3eb42033c9614cc0dfce0bdd8d7a9a
Pedro Civita <pedro.civita@gmail.com>
2023-09-11 11:18:02 -0300

    issue 5 finalizada

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -19,6 +19,20 @@
 #define BUT1_PIO_IDX 28
 #define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
 
+//Notas
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+
 // Funções
 void set_buzzer(){
 	pio_set(BUZZER_PIO_ID, BUZZER_PIO_IDX_MASK);
@@ -43,17 +57,28 @@ int get_selecao(){
 }
 
 void buzzer_test(int freq) {
-	int half_period_ms = 500 / freq;
+	
+	int meia_freq = freq/2;
 
 	while(1) {
 		pio_set(BUZZER_PIO, BUZZER_PIO_IDX_MASK);  // Liga o buzzer
-		delay_ms(half_period_ms);  // Aguarda meio período
-
+		delay_us(meia_freq);  // Aguarda meio período
 		pio_clear(BUZZER_PIO, BUZZER_PIO_IDX_MASK);  // Desliga o buzzer
-		delay_ms(half_period_ms);  // Aguarda meio período
+		delay_us(meia_freq);  // Aguarda meio período
 	}
 }
 
+void tone(int freq, int time){
+	
+	int meia_freq = freq/2;
+	
+	for(int i = 0; i < time; i++){
+		pio_set(BUZZER_PIO, BUZZER_PIO_IDX_MASK);  // Liga o buzzer
+		delay_us(meia_freq);  // Aguarda meio período
+		pio_clear(BUZZER_PIO, BUZZER_PIO_IDX_MASK);  // Desliga o buzzer
+		delay_us(meia_freq);  // Aguarda meio período
+	}
+}
 
 // Função init()
 void init(void) {
@@ -98,9 +123,12 @@ int main (void)
 	
 	// Escreve na tela um circulo e um texto
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-	gfx_mono_draw_string("Issue 4", 50,16, &sysfont);
+	gfx_mono_draw_string("Issue 5", 50,16, &sysfont);
 	
-	buzzer_test(500);
+	for (int freq=200; freq<5000; freq+=500){
+		tone(freq, 200 + freq/2);
+		delay_ms(200);
+	}
 	
 	/* Insert application code here, after the board has been initialized. */
 	while(1) {
